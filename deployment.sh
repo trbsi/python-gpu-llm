038078a56e214016b43f14b724d97623efa89f14
Robin <r.robin@gmx.com>
2026-01-28 15:43:08 +0100

    install bitsandbytes

--- deployment.sh
+++ deployment.sh
@@ -25,6 +25,7 @@ pip install transformers==5.0.0 sentencepiece==0.2.1 peft==0.18.1
 pip install fastapi==0.128.0 uvicorn[standard]==0.40.0
 pip install bugsnag==4.8.1
 pip install python-dotenv==1.2.1
+pip install -U bitsandbytes
 
 # -----------------------------
 # Navigate to your FastAPI app
